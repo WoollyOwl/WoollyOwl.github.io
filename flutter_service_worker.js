c12d74aa387bcf1a3140b7dad30e1c48f6e28804
WoollyOwl <patch.thomas@sky.com>
2023-11-03 13:39:12 +0000

    Modified typeWriterAnimation widget. Now the widget has a size which always fits the final string.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "77719b6e4c3f13154bc092eb82127e4d",
-"index.html": "212f631eecce0bc20805ce02cc7cc9b1",
-"/": "212f631eecce0bc20805ce02cc7cc9b1",
-"main.dart.js": "8e2a0656359fefba33295389db85e690",
+"index.html": "24d9dfbc28a72ee4ba12ac0b16f7bd2c",
+"/": "24d9dfbc28a72ee4ba12ac0b16f7bd2c",
+"main.dart.js": "0c7d0d170f77679d0b80eb86c38bd6e9",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
